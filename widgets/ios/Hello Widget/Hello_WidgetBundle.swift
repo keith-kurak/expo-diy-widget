7bf228f3df1d5314ac907436592e49c9cdd12ea8
Keith Kurak <keith.kurak@gmail.com>
2024-04-02 20:17:14 -0400

    trying to make a widget

---
+++ widgets/ios/Hello Widget/Hello_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Hello_WidgetBundle.swift
+//  Hello Widget
+//
+//  Created by Keith on 4/2/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Hello_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Hello_Widget()
+    }
+}
